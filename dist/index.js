4ef3d2faab580326387e92b769856cec82e23e3a
堀内亮佐 <ryosukehoriuti@yahoo.co.jp>
2016-02-23 02:24:46 +0900

    package

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var remote = require('remote');
-var fileUtil = remote.require('./lib/fileUtil');
-
-fileUtil.fetchReadmeList(function(err, matches) {
-  if(!err) document.write(matches.join());
-});
--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,15 @@
 'use strict';
-
 var app = require('app');
 var BrowserWindow = require('browser-window');
 
-require('crash-reporter').start();
+var crashReporter = require('electron').crashReporter;
+
+crashReporter.start({
+  productName: 'YourName',
+  companyName: 'YourCompany',
+  submitURL: 'https://your-domain.com/url-to-submit',
+  autoSubmit: true
+});
 
 var mainWindow = null;
 
@@ -13,15 +19,15 @@ app.on('window-all-closed', function() {
 });
 
 // var Tray = require('tray');
-// var appIcon = new Tray('file://' + __dirname + '/img/icon.png');
+// var appIcon = new Tray();
 
 app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
-});
+}); 
